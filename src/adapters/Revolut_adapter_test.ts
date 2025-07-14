18ff17aba8df4ae95f12812475d939419fd39728
Lucas Egidio <lucasegidio1@gmail.com>
2025-07-14 23:12:09 +0200

    refactor: new revolut adapter according to new csv structure

--- src/adapters/Revolut_adapter_test.ts
+++ src/adapters/Revolut_adapter_test.ts
@@ -15,82 +15,78 @@ Deno.test({
     assertEquals(
       brokerTransactions[0],
       <BrokerTransaction> {
-        date: new Date("2025-04-09"),
-        isin: "NL0010273215",
-        currency: CurrencyCode.EUR,
-        value: 4_852_15,
+        date: new Date("2025-03-14"),
+        isin: "US92826C8394",
+        currency: CurrencyCode.USD,
+        value: 224439,
       },
     );
     assertEquals(
       brokerTransactions[1],
       <BrokerTransaction> {
-        date: new Date("2025-06-25"),
-        isin: "NL0010273215",
-        currency: CurrencyCode.EUR,
-        value: 5_563_12,
+        date: new Date("2025-03-28"),
+        isin: "US92826C8394",
+        currency: CurrencyCode.USD,
+        value: 234598,
       },
     );
-
 
     assertEquals(
       brokerTransactions[2],
       <BrokerTransaction> {
-        date: new Date("2025-04-29"),
-        isin: "FI4000297767",
-        currency: CurrencyCode.EUR,
-        value: 3_593_98,
+        date: new Date("2025-04-08"),
+        isin: "US02079K3059",
+        currency: CurrencyCode.USD,
+        value: 537656,
       },
     );
     assertEquals(
       brokerTransactions[3],
       <BrokerTransaction> {
-        date: new Date("2025-06-26"),
-        isin: "FI4000297767",
-        currency: CurrencyCode.EUR,
-        value: 3_704_61,
+        date: new Date("2025-04-28"),
+        isin: "US02079K3059",
+        currency: CurrencyCode.USD,
+        value: 572344,
       },
     );
 
     assertEquals(
       brokerTransactions[4],
       <BrokerTransaction> {
-        date: new Date("2025-03-14"),
-        isin: "US92826C8394",
+        date: new Date("2025-04-09"),
+        isin: "NL0010273215",
         currency: CurrencyCode.EUR,
-        value: 2_063_33,
+        value: 485215,
       },
     );
     assertEquals(
       brokerTransactions[5],
       <BrokerTransaction> {
-        date: new Date("2025-03-28"),
-        isin: "US92826C8394",
+        date: new Date("2025-06-25"),
+        isin: "NL0010273215",
         currency: CurrencyCode.EUR,
-        value: 2_166_85,
+        value: 556312,
       },
     );
-
 
     assertEquals(
       brokerTransactions[6],
       <BrokerTransaction> {
-        date: new Date("2025-04-08"),
-        isin: "US02079K3059",
+        date: new Date("2025-04-29"),
+        isin: "FI4000297767",
         currency: CurrencyCode.EUR,
-        value: 4_897_75,
+        value: 359398,
       },
     );
     assertEquals(
       brokerTransactions[7],
       <BrokerTransaction> {
-        date: new Date("2025-04-28"),
-        isin: "US02079K3059",
+        date: new Date("2025-06-26"),
+        isin: "FI4000297767",
         currency: CurrencyCode.EUR,
-        value: 5_017_04,
+        value: 370461,
       },
     );
-
-
 
     assertEquals(brokerTransactions.length, 8);
   },
